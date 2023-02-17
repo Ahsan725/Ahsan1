82cbed705b4ba536123f8a782211e373748e1c61
Ahsan <sbaseer25@gmail.com>
2023-02-16 20:05:43 -0500

    First commit, all files comitted to the repository

---
+++ test.js
@@ -0,0 +1,51 @@
+const form = document.getElementById('form');
+const taskInput = document.getElementById('task');
+const dueDateInput = document.getElementById('dueDate');
+const prioritySelect = document.getElementById('priority');
+const tasks = document.getElementById('tasks');
+const audio = new Audio('tada.mp3'); // Replace path-to-sound-file with the actual URL of the sound file
+
+form.addEventListener('submit', addTask);
+
+function addTask(e) {
+  e.preventDefault();
+  if (taskInput.value === '') {
+    return;
+  }
+
+  const task = document.createElement('li');
+  task.innerHTML = `
+    <input type="checkbox">
+    <p>${taskInput.value}</p>
+    <span>Due: ${dueDateInput.value}</span>
+    <button type="button">Delete</button>
+  `;
+  task.classList.add(prioritySelect.value);
+
+  task.querySelector('input[type="checkbox"]').addEventListener('change', toggleDone);
+
+  task.querySelector('button').addEventListener('click', removeTask);
+  tasks.appendChild(task);
+
+  taskInput.value = '';
+  dueDateInput.value = '';
+  prioritySelect.value = '';
+}
+
+function toggleDone(e) {
+  const task = e.target.parentNode;
+  task.querySelector('p').classList.toggle('done');
+
+  if (task.querySelector('p').classList.contains('done')) {
+    task.querySelector('p').innerHTML = `<strike>${task.querySelector('p').textContent}</strike>`;
+    audio.play(); // Play the sound when the task is completed
+  } else {
+    task.querySelector('p').innerHTML = task.querySelector('p').textContent;
+    task.querySelector('p').style.backgroundColor = "";
+  }
+}
+
+function removeTask(e) {
+  const task = e.target.parentNode;
+  tasks.removeChild(task);
+}
